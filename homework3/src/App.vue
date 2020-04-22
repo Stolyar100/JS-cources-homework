751ff5924c7eebfe6930223caef26cee6760f326
Stolyar100 <olejer555@gmail.com>
2020-04-22 23:51:30 +0300

    Created the Vue project. Added select input and the ability to choose showed component

---
+++ homework3/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="select">
+      <select v-model="selected">
+        <option disabled value="">Виберіть завдання</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+      </select>
+    </div>
+    <span>Вибрано: {{ selected }}</span>
+    <converter v-if="selected == 2" />
+    <order v-if="selected == 3" />
+    <quiz v-if="selected == 4" />
+    <numberCheck v-if="selected == 5" />
+    <keyShift v-if="selected == 6" />
+    </div>
+</template>
+
+<script>
+import converter from './components/converter.vue'
+import order from './components/order.vue'
+import quiz from './components/quiz.vue'
+import numberCheck from './components/numberCheck.vue'
+import keyShift from './components/keyShift.vue'
+
+export default {
+  name: 'App',
+  components: {
+    converter,
+    order,
+    quiz,
+    numberCheck,
+    keyShift,
+  },
+
+  data: function() {
+    return {
+      selected: '',
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ homework3/src/components/converter.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Converter</h1>
+</template>
+
+<script>
+export default {
+  name: 'converter',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
