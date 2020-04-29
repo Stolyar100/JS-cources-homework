6186134b8bfd535209e606a54859dad918fc798e
Stolyar100 <olejer555@gmail.com>
2020-04-29 07:29:50 +0300

    finished first task(create obkect)

--- homeworkArrays/script.js
+++ homeworkArrays/script.js
@@ -1,5 +1,23 @@
 function firstTask() {
-
+    let animal = {
+        name: 'Cat',
+        weight: 5,
+        age: 4,
+        speed: 5,
+        getInfo: function() {
+            console.log(this);
+        },
+        setName: function(newName) {
+            this.name = newName;
+        },
+        calclTimeToThousand: function() {
+            let daysAmount = Math.ceil((1000 / this.speed) / 12);
+            console.log(`${this.name} пройде 1000 км за ${daysAmount} днів`);
+        },
+    }
+    animal.getInfo();
+    animal.setName('Devil');
+    animal.calclTimeToThousand();
 }
 
 function secondTask() {
