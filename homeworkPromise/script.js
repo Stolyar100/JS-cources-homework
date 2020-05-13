0b029acc82ca06cc1da9b8c45c454465a205d0f0
Stolyar100 <olejer555@gmail.com>
2020-05-13 14:18:52 +0300

    Renamed functions

--- homeworkPromise/script.js
+++ homeworkPromise/script.js
@@ -5,15 +5,15 @@ function firstTask() {
     showLoadScreen();
     fetch(base + currentResource)
         .then((res) => res.json())
-        .then((data) => addPeople(data.results));
+        .then((data) => addResource(data.results));
 }
 
-function peopleSearch(event) {
+function resourceSearch(event) {
     showLoadScreen();
     let request = 'https://swapi.dev/api/people/?search=';
     fetch(`${base}${currentResource}?search=${event.target.value}`)
         .then((res) => res.json())
-        .then((data) => addPeople(data.results));
+        .then((data) => addResource(data.results));
 }
 
 function setCurrentResource(event) {
@@ -22,7 +22,7 @@ function setCurrentResource(event) {
 }
 
 function showLoadScreen() {
-    clearPeopleList();
+    clearResourceList();
 
     let peopleEl = document.getElementById('people');
     let loadScreen = document.createElement('p');
@@ -30,31 +30,24 @@ function showLoadScreen() {
     peopleEl.append(loadScreen);
 }
 
-function addPeople(people) {
-    clearPeopleList();
+function addResource(people) {
+    clearResourceList();
     let peopleEl = document.getElementById('people');
-    people.forEach((person) => peopleEl.append(addPeopleCard(person)));
+    people.forEach((person) => peopleEl.append(addResourceCard(person)));
 }
 
-function addPeopleCard(person) {
+function addResourceCard(person) {
     let personCard = document.createElement('div');
     personCard.classList.add('people__card');
 
     for (property in person) {
-        personCard.append(addPersonProperty(property, person[property]));
+        personCard.append(addResourceProperty(property, person[property]));
     }
-
-    // personCard.append(addPersonProperty('name', person['name']));
-    // personCard.append(addPersonProperty('birth_year', person['birth_year']));
-    // personCard.append(addPersonProperty('height', person['height']));
-    // personCard.append(addPersonProperty('eye_color', person['eye_color']));
-    // personCard.append(addPersonProperty('mass', person['mass']));
-    // personCard.append(addPersonProperty('skin_color', person['skin_color']));
 
     return personCard;
 }
 
-function addPersonProperty(propertyName, propertyValue) {
+function addResourceProperty(propertyName, propertyValue) {
     let propertyBlock = document.createElement('div');
     propertyBlock.classList.add('people__property');
     let propertyNameBlock = document.createElement('h3');
@@ -71,7 +64,7 @@ function addPersonProperty(propertyName, propertyValue) {
     return propertyBlock;
 }
 
-function clearPeopleList() {
+function clearResourceList() {
     let peopleEl = document.getElementById('people');
     peopleEl.innerText = '';
 }
